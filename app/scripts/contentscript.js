ea7a1b45fc7943f84e834187c0fc8679f1539130
Tyler Crammond <tylerchc92@gmail.com>
2016-12-12 12:19:24 +0000

    Add fields to manually enter template content
    GitHub appears to no longer allow requests to different origins.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,84 +1,85 @@
 'use strict';
 (function () {
-    var defaultUrl = 'https://raw.github.com/sprintly/sprint.ly-culture/master/pr-template.md';
-    var options, isCustom;
+  var defaultUrl = 'https://raw.github.com/sprintly/sprint.ly-culture/master/pr-template.md';
+  var options, isCustom;
 
-    var isGH = window.location.href.match(/github.com/);
-    var isBB = window.location.href.match(/bitbucket.org/);
+  var isGH = window.location.href.match(/github.com/);
+  var isBB = window.location.href.match(/bitbucket.org/);
 
-    loadOptions(getTemplate);
+  loadOptions(getTemplate);
 
-    function loadOptions(cb) {
-        chrome.storage.sync.get({
-            githubEnabled: true,
-            githubTemplateUrl: defaultUrl,
-            bitbucketEnabled: true,
-            bitbucketTemplateUrl: defaultUrl,
+  function loadOptions (cb) {
+    chrome.storage.sync.get({
+      githubEnabled: true,
+      githubTemplateUrl: defaultUrl,
+      githubTemplateContent: '',
+      bitbucketEnabled: true,
+      bitbucketTemplateUrl: defaultUrl,
+      bitbucketTemplateContent: '',
 
-            customEnabled: true,
-            customTemplateUrl: defaultUrl,
-            customRepoRegex: '',
-            customRepoDescriptionID: ''
+      customEnabled: true,
+      customTemplateUrl: defaultUrl,
+      customRepoRegex: '',
+      customRepoDescriptionID: ''
+    }, function (items) {
+      options = items;
+      cb();
+    });
+  }
 
-        }, function (items) {
-            options = items;
-            cb();
-        });
+  function insertTemplate (template) {
+    var el = null;
+
+    if (isGH && options.githubEnabled) {
+      el = document.getElementById('pull_request_body');
+    } else if (isBB && options.bitbucketEnabled) {
+      el = document.getElementById('id_description');
+    } else if(isCustom && options.customEnabled && options.customRepoDescriptionID) {
+      el = document.getElementById(options.customRepoDescriptionID.toString());
     }
 
-    function insertTemplate(template) {
-        var el = null;
+    if (el !== null) {
+      /*
+       Bitbucket clears out the PR description field / put in its own content based on commits to be merged.
+       We'll append the content.
+      */
+      if (isBB || isCustom) {
+        setTimeout(function() {
+          el.value = el.value + (el.value && el.value.length ? '\r\n' : '') + template;
+        }, 1000);
+      } else {
+        el.value = template;
+      }
+    }
+  }
 
-        if (isGH && options.githubEnabled) {
-            el = document.getElementById('pull_request_body');
-        } else if (isBB && options.bitbucketEnabled) {
-            el = document.getElementById('id_description');
-        } else if(isCustom && options.customEnabled && options.customRepoDescriptionID) {
-            el = document.getElementById(options.customRepoDescriptionID.toString());
-        }
+  function getTemplate() {
+    var templateToLoad;
+    var xhr = new XMLHttpRequest();
 
-        if (el !== null) {
-            /*
-             Bitbucket clears out the PR description field / put in its own content based on commits to be merged.
-             We'll append the content.
-            */
-            if (isBB || isCustom) {
-                setTimeout(function() {
-                    el.value = el.value + (el.value && el.value.length ? '\r\n' : '') + template;
-                }, 1000);
-            } else {
-                el.value = template;
-            }
-        }
+    isCustom = (options.customRepoRegex) ? new RegExp(options.customRepoRegex).test(window.location.href) : false;
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        insertTemplate(xhr.responseText);
+      }
+    };
+
+    if (isGH) {
+      // GitHub cannot retrieve from external URL due to cross origin rules.
+      insertTemplate(options.githubTemplateContent);
+    } else if (isBB) {
+      if (options.bitbucketTemplateContent) return insertTemplate(options.bitbucketTemplateContent);
+      templateToLoad = options.bitbucketTemplateUrl;
+    } else if (isCustom) {
+      if (options.customTemplateContent) return insertTemplate(options.customTemplateContent);
+      templateToLoad = options.customTemplateUrl;
     }
 
-    function getTemplate() {
-        var templateToLoad,
-            xhr = new XMLHttpRequest();
-
-        isCustom = (options.customRepoRegex) ? new RegExp(options.customRepoRegex).test(window.location.href) : false;
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                insertTemplate(xhr.responseText);
-            }
-        };
-
-        if(isGH) {
-            templateToLoad = options.githubTemplateUrl;
-        } else if(isBB) {
-            templateToLoad = options.bitbucketTemplateUrl;
-        } else if(isCustom) {
-            templateToLoad = options.customTemplateUrl;
-        }
-
-        if(templateToLoad) {
-            xhr.open("GET", (templateToLoad), true);
-            xhr.send();
-        }
-
+    if (templateToLoad) {
+      xhr.open("GET", (templateToLoad), true);
+      xhr.send();
     }
+
+  }
 })();
-
-
-
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,75 +1,84 @@
 'use strict';
 (function () {
-    var defaultUrl = 'https://raw.github.com/sprintly/sprint.ly-culture/master/pr-template.md';
+  var defaultUrl = 'https://raw.github.com/sprintly/sprint.ly-culture/master/pr-template.md';
 
-    // Saves options to chrome.storage
-    function save_options() {
+  // Saves options to chrome.storage
+  function save_options() {
 
-        var githubEnabled = document.getElementById('githubEnabled').checked;
-        var githubTemplateUrl = document.getElementById('githubTemplateUrl').value;
-        var bitbucketEnabled = document.getElementById('bitbucketEnabled').checked;
-        var bitbucketTemplateUrl = document.getElementById('bitbucketTemplateUrl').value;
-        
-        var customEnabled = document.getElementById('customRepoEnabled').checked;
-        var customTemplateUrl = document.getElementById('customRepoTemplateUrl').value;
-        var customRepoRegex = document.getElementById('customRepoRegex').value;
-        var customRepoDescriptionID = document.getElementById('customRepoDescriptionID').value;
+    var githubEnabled = document.getElementById('githubEnabled').checked;
+    var githubTemplateUrl = document.getElementById('githubTemplateUrl').value;
+    var githubTemplateContent = document.getElementById('githubTemplateContent').value;
+    var bitbucketEnabled = document.getElementById('bitbucketEnabled').checked;
+    var bitbucketTemplateContent = document.getElementById('bitbucketTemplateContent').value
+    var bitbucketTemplateUrl = document.getElementById('bitbucketTemplateUrl').value;
 
-        chrome.storage.sync.set({
-            githubEnabled: githubEnabled,
-            githubTemplateUrl: githubTemplateUrl ? githubTemplateUrl : defaultUrl,
-            bitbucketEnabled: bitbucketEnabled,
-            bitbucketTemplateUrl: bitbucketTemplateUrl ? bitbucketTemplateUrl : defaultUrl,
+    var customEnabled = document.getElementById('customRepoEnabled').checked;
+    var customTemplateContent = document.getElementById('customRepoTemplateContent').value;
+    var customTemplateUrl = document.getElementById('customRepoTemplateUrl').value;
+    var customRepoRegex = document.getElementById('customRepoRegex').value;
+    var customRepoDescriptionID = document.getElementById('customRepoDescriptionID').value;
 
-            customEnabled: customEnabled,
-            customTemplateUrl: customTemplateUrl ? customTemplateUrl : defaultUrl,
-            customRepoRegex: customRepoRegex ? customRepoRegex : '',
-            customRepoDescriptionID: customRepoDescriptionID ? customRepoDescriptionID : ''
+    chrome.storage.sync.set({
+      githubEnabled: githubEnabled,
+      githubTemplateUrl: githubTemplateUrl ? githubTemplateUrl : defaultUrl,
+      githubTemplateContent: githubTemplateContent || '',
+      bitbucketEnabled: bitbucketEnabled,
+      bitbucketTemplateUrl: bitbucketTemplateUrl ? bitbucketTemplateUrl : defaultUrl,
+      bitbucketTemplateContent: bitbucketTemplateContent || '',
+      customEnabled: customEnabled,
+      customTemplateUrl: customTemplateUrl ? customTemplateUrl : defaultUrl,
+      customTemplateContent: customTemplateContent || '',
+      customRepoRegex: customRepoRegex ? customRepoRegex : '',
+      customRepoDescriptionID: customRepoDescriptionID ? customRepoDescriptionID : ''
 
-        }, function () {
+    }, function () {
+      // Update status to let user know options were saved.
+      var result = document.getElementById('save-result');
+      result.className = result.className.replace(/\bhide\b/,'');
 
-            // Update status to let user know options were saved.
-            var result = document.getElementById('save-result');
-            result.className = result.className.replace(/\bhide\b/,'');
+      setTimeout(function () {
+        result.className = result.className + ' hide';
+      }, 1500);
+    });
 
-            setTimeout(function () {
-                result.className = result.className + ' hide';
-            }, 1500);
+  }
 
-        });
+  // Restores select box and checkbox state using the preferences
+  // stored in chrome.storage.
+  function restore_options() {
 
-    }
+    chrome.storage.sync.get({
+      githubEnabled: true,
+      githubTemplateUrl: defaultUrl,
+      githubTemplateContent: '',
+      bitbucketEnabled: true,
+      bitbucketTemplateUrl: defaultUrl,
+      bitbucketTemplateContent: '',
 
-    // Restores select box and checkbox state using the preferences
-    // stored in chrome.storage.
-    function restore_options() {
+      customEnabled: true,
+      customTemplateUrl: defaultUrl,
+      customTemplateContent: '',
+      customRepoRegex: '',
+      customRepoDescriptionID: ''
 
-        chrome.storage.sync.get({
-            githubEnabled: true,
-            githubTemplateUrl: defaultUrl,
-            bitbucketEnabled: true,
-            bitbucketTemplateUrl: defaultUrl,
+    }, function (items) {
+      document.getElementById('githubEnabled').checked = items.githubEnabled;
+      document.getElementById('githubTemplateUrl').value = items.githubTemplateUrl;
+      document.getElementById('githubTemplateContent').value = items.githubTemplateContent;
+      document.getElementById('bitbucketEnabled').checked = items.bitbucketEnabled;
+      document.getElementById('bitbucketTemplateUrl').value = items.githubTemplateUrl;
+      document.getElementById('bitbucketTemplateContent').value = items.bitbucketTemplateContent;
 
-            customEnabled: true,
-            customTemplateUrl: defaultUrl,
-            customRepoRegex: '',
-            customRepoDescriptionID: ''
+      document.getElementById('customRepoEnabled').checked = items.customEnabled;
+      document.getElementById('customRepoTemplateUrl').value = items.customTemplateUrl;
+      document.getElementById('customRepoTemplateContent').value = items.customTemplateContent;
+      document.getElementById('customRepoRegex').value = items.customRepoRegex;
+      document.getElementById('customRepoDescriptionID').value = items.customRepoDescriptionID;
 
-        }, function (items) {
-            document.getElementById('githubEnabled').checked = items.githubEnabled;
-            document.getElementById('githubTemplateUrl').value = items.githubTemplateUrl;
-            document.getElementById('bitbucketEnabled').checked = items.bitbucketEnabled;
-            document.getElementById('bitbucketTemplateUrl').value = items.githubTemplateUrl;
-            
-            document.getElementById('customRepoEnabled').checked = items.customEnabled;
-            document.getElementById('customRepoTemplateUrl').value = items.customTemplateUrl;
-            document.getElementById('customRepoRegex').value = items.customRepoRegex;
-            document.getElementById('customRepoDescriptionID').value = items.customRepoDescriptionID;
+    });
 
-        });
+  }
 
-    }
-
-    document.addEventListener('DOMContentLoaded', restore_options);
-    document.getElementById('save').addEventListener('click', save_options);
+  document.addEventListener('DOMContentLoaded', restore_options);
+  document.getElementById('save').addEventListener('click', save_options);
 })();
